62a33875f13964540c0f103e669e441bd7e5d862
Moria Hamami <moriahamami@gmail.com>
2023-03-21 22:14:10 +0200

    Fix audio  bug

--- frontend/src/cmps/game-board.tsx
+++ frontend/src/cmps/game-board.tsx
@@ -1,86 +1,73 @@
 import { useEffect, useState, useRef } from 'react'
 import { gameService } from '../services/game.service'
 import { utilService } from '../services/util.service'
+const redSoundUrl = require('../assets/audio/redSound.mp3')
+const blueSoundUrl = require('../assets/audio/blueSound.mp3')
+const greenSoundUrl = require('../assets/audio/greenSound.mp3')
+const yellowSoundUrl = require('../assets/audio/yellowSound.mp3')
+const loseSoundUrl = require('../assets/audio/loseSound.mp3')
+
+const audioMap: { [key: string]: HTMLAudioElement } = {
+    green: new Audio(greenSoundUrl),
+    red: new Audio(redSoundUrl),
+    yellow: new Audio(yellowSoundUrl),
+    blue: new Audio(blueSoundUrl)
+}
 
 function GameBoard() {
     const [markedColor, setMarkedColor] = useState<string>('')
     const [simonSeq, setSimonSeq] = useState<string[]>([])
     const [userSeq, setUserSeq] = useState<string[]>([])
     const [isUserTurn, setIsUserTurn] = useState<boolean>(false)
-    const intervalId = useRef<any>(null)
-    const timeoutId = useRef<any>(null)
-    // simonSeq
-    // userSeq
-    // isSeqPlaying
-    // userStack
-    // currColor
+    // const intervalId = useRef<any>(null)
+    // const timeoutId = useRef<any>(null)
 
     useEffect(() => {
         if (!isUserTurn) playSeq()
     }, [isUserTurn])
 
-    // const seqStack = []
-    // colorToSelect
-    // seqStack.push(color)
-    // setColorToSelect(color)
-    // currColor 
     async function playSeq() {
         const nextCorrectColor = gameService.getRandColor()
         simonSeq.push(nextCorrectColor)
-        // setSimonSeq(prevSeq => prevSeq ? [...prevSeq, nextCorrectColor] : [nextCorrectColor])
         for (let i = 0; i < simonSeq.length; i++) {
             await utilService.wait(500)
-
-            // let i = 0
-            // setMarkedColor(simonSeq[i])
-            // i++
-            // intervalId.current = setInterval(() => {
-            setMarkedColor(simonSeq[i])
-            // i++
-            await utilService.wait(1000)
-            // If this is the last color in seq
-            // if (i >= simonSeq.length) {
-            //     timeoutId.current = setTimeout(() => {
-            //         clearInterval(intervalId.current)
+            const currColor = simonSeq[i]
+            setMarkedColor(currColor)
+            const currSound = audioMap[currColor]
+            currSound.play()
+            await utilService.wait(500)
             setMarkedColor('')
-            // setIsUserTurn(true)
-            // clearTimeout(timeoutId.current)
-            // }, 2000)
         }
-        // }, 2000)
         setIsUserTurn(true)
-        // }
-
-
     }
 
     async function colorSelected(selectedColor: string) {
         if (!isUserTurn) return
         userSeq.push(selectedColor)
-        // setUserSeq(prevSeq => prevSeq ? [...prevSeq, selectedColor] : [selectedColor])
-        // simonSeq.push(selectedColor)
         const simonsColor = simonSeq[userSeq.length - 1]
 
-        setMarkedColor(simonsColor)
-        await utilService.wait(500)
-        setMarkedColor('')
-
         if (selectedColor === simonsColor) {
+            setMarkedColor(selectedColor)
+            const currSound = audioMap[selectedColor]
+            currSound.play()
+            await utilService.wait(500)
+            setMarkedColor('')
             // If the whole simon seq was correct
             if (userSeq.length === simonSeq.length) {
                 setUserSeq([])
                 setIsUserTurn(false)
-                // playSeq()
             }
         } else {
-
-
+            setMarkedColor(simonsColor)
+            const currSound = new Audio(loseSoundUrl)
+            currSound.play()
+            await utilService.wait(500)
+            setMarkedColor('')
             // If the user is incorrect, the game restarts
             setIsUserTurn(false)
             setUserSeq([])
             setSimonSeq([])
             // show modal later
-            // playSeq()
         }
     }
 
@@ -89,22 +76,13 @@ function GameBoard() {
         else return `${color}-btn`
     }
 
-    // function setMarkedColorWTime(color: string) {
-    //     setMarkedColor(color)
-    //     timeoutId.current = setTimeout(() => {
-    //         setMarkedColor('')
-    //         clearTimeout(timeoutId.current)
-    //     }, 2000)
-    // }
-
     return (
         <div className="game-board">
-            {/* {'marked:' + markedColor} */}
             <button className={getClassName('green')} onClick={() => colorSelected('green')}>Green</button>
             <button className={getClassName('red')} onClick={() => colorSelected('red')}>Red</button>
             <button className={getClassName('blue')} onClick={() => colorSelected('blue')}>Blue</button>
             <button className={getClassName('yellow')} onClick={() => colorSelected('yellow')}>Yellow</button>
-            {'simonSeq:' + JSON.stringify(simonSeq)}
+            {/* {'simonSeq:' + JSON.stringify(simonSeq)} */}
         </div>
     )
 
